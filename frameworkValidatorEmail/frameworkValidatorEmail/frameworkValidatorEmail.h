4d69030f6ea9edb652ecf011ed0734c331d363c1
terry2262001 <terynguyen2262001@gmail.com>
2024-05-01 01:23:52 +0700

    update: framework

---
+++ frameworkValidatorEmail/frameworkValidatorEmail/frameworkValidatorEmail.h
@@ -0,0 +1,18 @@
+//
+//  frameworkValidatorEmail.h
+//  frameworkValidatorEmail
+//
+//  Created by rta on 4/30/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for frameworkValidatorEmail.
+FOUNDATION_EXPORT double frameworkValidatorEmailVersionNumber;
+
+//! Project version string for frameworkValidatorEmail.
+FOUNDATION_EXPORT const unsigned char frameworkValidatorEmailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <frameworkValidatorEmail/PublicHeader.h>
+
+
